280a1cdb1fed457dae13e8c844fb9137a52587ba
Brent Page <brentfpage@Brents-MacBook-Pro.local>
2021-05-04 10:11:03 -0700

    triple einsum

--- lusee_sky.py
+++ lusee_sky.py
@@ -13,7 +13,7 @@ from datetime import timedelta
 from scipy.interpolate import interp1d
 
 # gsm
-gsm = GlobalSkyModel()
+gsm = GlobalSkyModel(interpolation = 'cubic')
 gsm16 = GlobalSkyModel2016()
 
 
@@ -246,21 +246,20 @@ def get_map_pixel_local_vecs(utc_times, beam_idxs=None, map_nside=512, nest=Fals
         galactic_to_moon_rots[i] = spice.pxform(
             "GALACTIC", "MOON_ME", spice.datetime2et(time)
         )
-    galactic_to_local_rots = np.einsum(
-        "ij,kjl->kil", local_to_moon_coords.T, galactic_to_moon_rots
-    )
 
     galac_vecs = np.stack(
         hp.pix2vec(map_nside, np.arange(hp.nside2npix(map_nside)), nest=nest)
     ).T
 
     if not (beam_idxs is None):
-        # transform a different set of pixel positions for each time
         galac_vecs = galac_vecs[beam_idxs]
-        local_vecs = np.einsum("ijk,ilk->ilj", galactic_to_local_rots, galac_vecs)
+        # transform a different set of pixel positions for each time
+        # t: time, p: pixel
+        local_vecs = np.einsum("ij,tjl,tpl->tpi", local_to_moon_coords.T, galactic_to_moon_rots, galac_vecs)
+        
     else:
         # transform the positions of all pixels for each time
-        local_vecs = np.einsum("ijk,lk->ilj", galactic_to_local_rots, galac_vecs)
+        local_vecs = np.einsum("ij,tjl,pl->tpi", local_to_moon_coords.T, galactic_to_moon_rots, galac_vecs)
 
     return galac_vecs, local_vecs
 
@@ -384,15 +383,15 @@ def time_freq_K(
     return KK
 
 
-def drive(minutes = 240, NS = 60, EW = 5, nside = 128, time_chunk = 20, plot = False):
+def drive(minutes = 240, threshold = .01, NS = 60, EW = 5, nside = 128, time_chunk = 20, plot = False):
     sta = timer()
     utc_times = np.arange(
         datetime(2024, 3, 21, 21), datetime(2024, 4, 5, 11), timedelta(minutes=minutes)
     ).astype(datetime)
     KK = time_freq_K(
         utc_times,
-        freqs=np.arange(10, 71, 10),
-#         freqs=np.arange(20, 51, 1),
+#         freqs=np.arange(10, 71, 10),
+        freqs=np.arange(20, 50, .5),
         NS_20MHz_beam_stdev_degr=NS,
         EW_20MHz_beam_stdev_degr=EW,
         map_nside=nside,
@@ -400,7 +399,7 @@ def drive(minutes = 240, NS = 60, EW = 5, nside = 128, time_chunk = 20, plot = F
         plot = plot
     )
     sto = timer()
-#     print(sto - sta)
+    print(sto - sta)
 
 
 # utc_time: datetime
